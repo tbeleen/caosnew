f25a70a9c5a9d9d0de268252fe3f5daf46108ad0
tamara <t.beleenmiranda@gmail.com>
2024-11-27 18:18:19 -0300

    seconds commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,15 +89,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
 
     }
-}"""
+}
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST' : 'aws-0-sa-east-1.pooler.supabase.com',
@@ -108,7 +108,7 @@ DATABASES = {
         
         
     }
-}
+}"""
 
 
 # Password validation
